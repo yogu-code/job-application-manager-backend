45e74539e471290cc465329540832431c7b83212
yogu-code <yogeshrane019@gmail.com>
2025-08-31 22:28:04 +0530

    ADD CORS

--- src/server.js
+++ src/server.js
@@ -9,10 +9,18 @@ import jobRoutes from './route/Job.route.js';
 
 
 const { json } = pkg;
-dotenv.config();
+dotenv.config(
+    
+);
 connectDB();
 const app = express();
 const PORT = process.env.PORT 
+app.use(
+  cors({
+    origin: "http://localhost:3000", // your Next.js frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(json());
 app.use(bodyParser.urlencoded({ extended: true }));
